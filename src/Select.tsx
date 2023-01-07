3f614afd8ef78007fd264aef8821750bcfafa444
gunjan1909 <bhanarkaranita@gmail.com>
2023-01-07 17:42:15 +0530

    .

--- src/Select.tsx
+++ src/Select.tsx
@@ -5,16 +5,19 @@ export type SelectOption = {
   label: string;
   value: string | number;
 };
+
 type MultipleSelectProps = {
   multiple: true;
   value: SelectOption[];
   onChange: (value: SelectOption[]) => void;
 };
+
 type SingleSelectProps = {
   multiple?: false;
   value?: SelectOption;
   onChange: (value: SelectOption | undefined) => void;
 };
+
 type SelectProps = {
   options: SelectOption[];
 } & (MultipleSelectProps | SingleSelectProps);
